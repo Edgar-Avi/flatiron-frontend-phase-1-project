cf06ca88c9b69c77d1288ba979f87f0f28281c70
Edgar Avila <edgaravila01@yahoo.com>
2023-07-18 13:00:01 -0700

    added notes on main

--- main.js
+++ main.js
@@ -20,9 +20,30 @@ const getMealCategories = async () => {
 };
 
 //render meal categories data to dom
-const renderMealCategories = (mealCategoriesObj) => {
+const renderMealCategories = (mealCategoriesArray) => {
     console.log('renderMealCategories');
-    console.table(mealCategoriesObj);
+    console.table(mealCategoriesArray);
+
+// find and select in a UL our meal categories ID dom element to append my data into
+
+// for each element in our meal categories array
+    // create a wrapping element maybe <article> class of card
+    // appendChild the article.card to selected DOM element
+    // display the category name, image, and description
+        // image
+            // create image element
+            // set img src to category thumbnail url
+            // give class of categoty-thumbnail url
+            // give it a mobile friendly max width of 300px
+            // appendChild the img to our card
+        // name
+            // create an h4 element
+            // set h4 element textContent to be meal category name
+            // appendChild the h4 to our card
+        // description
+            // create an <p> element
+            // set p element textContent to be meal category description
+            // appendChild the p to our card    
 };
 
 //function to call when form input is given focus
