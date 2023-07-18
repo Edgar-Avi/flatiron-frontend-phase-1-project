5b7660a2b9c3568d446eaa7d960c603ba86a1d89
Edgar Avila <edgaravila01@yahoo.com>
2023-07-18 10:01:04 -0700

    fetch categories data

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('connected')
+console.log('main.js connected')
+
+const searchTermsInput = document.body.querySelector("#search-terms")
+
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+    
+    try {
+        const response = await fetch(mealCategoriesApiURL)
+        const data = response.json()
+        console.log(`data: `, data);
+    } catch (error) {
+        console.log(error)
+        alert('Something went wrong,try again')
+    }
+}
+const handleFormInputFocus = async () => {
+    console.log(`focus occured`);
+
+    await getMealCategories();
+};
+
+searchTermsInput.addEventListener('focus', handleFormInputFocus);
